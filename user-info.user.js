fb4390ee8f2fd88d3dd2f0f59c2de2d79eae30b1
Emmie Maeda <emmie.maeda@gmail.com>
2025-06-19 04:04:13 -0400

    Set up user profile fetch logic.

--- user-info.user.js
+++ user-info.user.js
@@ -16,6 +16,13 @@
 // @include     https://scp-wiki.wikidot.com/system:user/*
 // ==/UserScript==
 
+let USER_ID;
+let USERNAME;
+let WIKIDOT_JOIN_DATE;
+let WIKIDOT_DAYS;
+let SITE_JOIN_DATE = null;
+let SITE_DAYS = null;
+
 function getUserId() {
   const element = document.querySelector('a.btn.btn-default.btn-xs');
   const userIdRegex = /https?:\/\/www\.wikidot\.com\/account\/messages#\/new\/(\d+)/;
@@ -23,22 +30,83 @@ function getUserId() {
   return matches[1];
 }
 
-function addDescriptionEntry(descriptionList, key, value, insertIndex) {
-  const insertBeforeElement = descriptionList.children[insertIndex];
-  if (!insertBeforeElement) {
-    alert(`Invalid insertion index: ${insertIndex} (only ${descriptionList.children.length} items)`);
-    return;
+async function fetchUserInfo(userId) {
+  // Setup
+  const dateRegex = /([^,]+, [0-9]{2}:[0-9]{2}) \(([0-9]+ days) .+\)/;
+  const userInfoPromise = new Promise(resolve => (
+    OZONE.ajax.requestModule('users/UserInfoWinModule', {user_id: userId}, resolve)
+  ));
+
+  // Fetch request HTML and write into hidden element for querying
+  const element = document.createElement('html');
+  element.innerHTML = await userInfoPromise.body;
+
+  // Get username from header
+  USERNAME = element.querySelector('h1').innerText;
+
+  // Get data from fields
+  const fields = element.querySelectorAll('tr');
+  let i;
+
+  // The first date field is the Wikidot join date
+  for (i = 0; i < fields.length; i++) {
+    const field = fields[i];
+    const matches = field.innerText.match(dateRegex);
+    if (matches === null) {
+      // This isn't a date, keep moving
+      continue;
+    }
+
+    // This *is* a date, so extract the fields and quit searching
+    // (but save index so we can get the second one)
+    WIKIDOT_JOIN_DATE = new Date(matches[1]);
+    WIKIDOT_DAYS = matches[2];
   }
 
+  // The second date field (if it exists) is the site join date
+  let siteJoinDate = null;
+  let siteDays = null;
+  for (; i < fields.length; i++) {
+    // Same logic, as above
+    const field = fields[i];
+    const matches = field.innerText.match(dateRegex);
+    if (matches === null) {
+      continue;
+    }
+
+    SITE_JOIN_DATE = new Date(matches[1]);
+    SITE_DAYS = matches[2];
+  }
+}
+
+function addDescriptionEntry(descriptionList, key, value, insertIndex) {
   const dt = document.createElement('dt');
   const dd = document.createElement('dd');
   dt.innerText = key;
   dd.innerText = value;
-  descriptionList.insertBefore(dt, insertBeforeElement);
-  descriptionList.insertBefore(dd, insertBeforeElement);
+
+  if (insertIndex === -1) {
+    // -1 means append to end
+    descriptionList.appendChild(dt);
+    descriptionList.appendChild(dd);
+  } else {
+    // 0+ means add at that index
+    const insertBeforeElement = descriptionList.children[insertIndex];
+    if (!insertBeforeElement) {
+      alert(`Invalid insertion index: ${insertIndex} (only ${descriptionList.children.length} items)`);
+      return;
+    }
+
+    descriptionList.insertBefore(dt, insertBeforeElement);
+    descriptionList.insertBefore(dd, insertBeforeElement);
+  }
 }
 
 async function main() {
+  // Fetch data (one-time)
+  USER_ID = getUserId();
+  fetchUserInfo(USER_ID);
+
   // TODO add listener to re-add fields when switching back to "profile"
 
   const infoElement = document.getElementById('user-info-area');
@@ -53,9 +121,12 @@ async function main() {
     return;
   }
 
-  // Add user ID
-  const userId = getUserId();
-  addDescriptionEntry(descriptionList, 'User ID:', userId, 0);
+  // Add new info elements
+  addDescriptionEntry(descriptionList, 'User ID:', USER_ID, 0);
+  addDescriptionEntry(descriptionList, 'User name:', USERNAME, 1);
+
+  const infoLine = `${username} (W: ${WIKIDOT_DAYS}, S: ${SITE_DAYS || 'none'}, ID: ${USER_ID})`;
+  addDescriptionEntry(descriptionList, 'Info line:', infoLine, -1);
 }
 
 main();
